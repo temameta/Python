cdf27e586ac6b4b18ff17a491ffa1a9afc79af95
Tema <62208316+temameta@users.noreply.github.com>
2023-11-22 00:37:03 +0300

    Update ThirdCase.py

--- ThirdCase.py
+++ ThirdCase.py
@@ -1,2 +1,2 @@
 meters = float(input())
-print("The final price is: %.2f $" % (meters*7.61-(meters*7.61*0.18)), "\nThe discount is: %.2f $" % (meters*7.61*0.18))
+print("The final price is: %.2f $" % (meters * 7.61 - (meters * 7.61 * 0.18)), "\nThe discount is: %.2f $" % (meters * 7.61 * 0.18))
