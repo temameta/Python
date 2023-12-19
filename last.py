55821bbacdd63b6db0bea2442213875c528f308c
Tema <62208316+temameta@users.noreply.github.com>
2023-12-20 02:20:07 +0300

    Update last.py

--- last.py
+++ last.py
@@ -6,7 +6,6 @@ while inputString.lower() != "stop":
         num = int(inputString)
         d=0
         if num >= 0:
-            prime = True
             for i in range(2, int(num**0.5)+1):
                 if num % i == 0:
                     d+=1
