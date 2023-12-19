d9d09820fd52975d8cc7dcbaef292f49de87f371
Tema <62208316+temameta@users.noreply.github.com>
2023-12-20 02:12:52 +0300

    Update ravniechisla.py

--- ravniechisla.py
+++ ravniechisla.py
@@ -1,27 +1,33 @@
 def isInteger(userInput):
     try:
-        userInput = int(userInput)
+        int(userInput)
     except ValueError:
         return False
     return True
     
 n = input()
-flag = True 
+flag = False 
 if isInteger(n): 
     n = int(n)
-    couples = []
-    a = input()
-    b = input()
-    for i in range(n*2):
+    couplesSum = []
+    for i in range(n):
+        a = input()
         while not(isInteger(a)):
             print("Неверный ввод")
-            a = int(input())
+            a = input()
+        b = input()
         while not(isInteger(b)):
             print("Неверный ввод")
-            b = int(input())
-        
-        couples[i] = a
+            b = input()
+        couplesSum.append(int(a)+int(b))
+    maxValue = []
     for i in range(1, n):
-        if a[i] < 
-        
+        if couplesSum[i] != couplesSum[i-1]:
+            maxValue.append(couplesSum[i-1] - couplesSum[i])
+    if len(maxValue) == 0:
+        print(f'Yes, value = {couplesSum[0]}')
+    else:
+        print(f'No, maxdiff = {max(maxValue)}')
+else:
+    print("ERROR")
             
